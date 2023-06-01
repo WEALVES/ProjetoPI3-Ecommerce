7c9cc299ffbfb379cb341c3a973a3ab2e1ab339a
Lucas <kinhasjesus17@gmail.com>
2023-05-31 21:00:23 -0300

    Criando tela de Alterar, falta Estilizar

---
+++ techStore/src/main/java/br/com/techstore/servlet/AlterProdutoServlet.java
@@ -0,0 +1,117 @@
+package br.com.techstore.servlet;
+
+
+import br.com.techstore.dao.ProdutoDao;
+import br.com.techstore.model.Produto;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.File;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.apache.commons.fileupload.servlet.ServletFileUpload.isMultipartContent;
+
+@WebServlet("/create-produto")
+public class CreateProdutoServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+
+        Map<String, String> parameters = uploadImage(req);
+        System.setProperty("file.encoding","UTF-8");
+        int id = Integer.parseInt(parameters.get("id"));
+        String nome = parameters.get("nome");
+        Double preco = Double.valueOf(parameters.get("preco"));
+        String categoria = parameters.get("categoria");
+        String marca = parameters.get("marca");
+        String descricao = parameters.get("descricao");
+        int quantidade = Integer.parseInt(parameters.get("quantidade"));
+        String imagePath = parameters.get("image");
+
+
+        Produto produto = new Produto(id, nome, categoria, marca, preco, descricao, quantidade, imagePath);
+
+        ProdutoDao produtoDao = new ProdutoDao();
+
+
+        if(nome.isBlank()){
+
+            produtoDao.createProduto(produto);
+
+
+        }else{
+            produtoDao.updateProduto(produto);
+
+        }
+
+
+        resp.sendRedirect("/find-all-produtos");
+
+
+
+
+    }
+
+    private Map<String, String> uploadImage(HttpServletRequest httpServletRequest) {
+
+        System.setProperty("file.encoding","UTF-8");
+        Map<String, String> requestParameters = new HashMap<>();
+
+        if (isMultipartContent(httpServletRequest)) {
+
+            try {
+                DiskFileItemFactory diskFileItemFactory = new DiskFileItemFactory();
+
+                List<FileItem> fileItems = new ServletFileUpload(diskFileItemFactory).parseRequest(httpServletRequest);
+
+                for (FileItem fileItem : fileItems) {
+
+                    checkFieldType(fileItem, requestParameters);
+
+                }
+
+            } catch (Exception ex) {
+
+                requestParameters.put("image", "assets/imgProduct/default-car.jpg");
+
+            }
+
+        }
+
+        return requestParameters;
+
+    }
+
+    private void checkFieldType(FileItem item, Map requestParameters) throws Exception {
+
+        if (item.isFormField()) {
+
+            requestParameters.put(item.getFieldName(), item.getString());
+
+        } else {
+
+            String fileName = processUploadedFile(item);
+            requestParameters.put("image", "assets/imgProduct/".concat(fileName));
+
+        }
+
+    }
+
+    private String processUploadedFile(FileItem fileItem) throws Exception {
+        Long currentTime = new Date().getTime();
+        String fileName = currentTime.toString().concat("-").concat(fileItem.getName().replace(" ", ""));
+        String filePath = this.getServletContext().getRealPath("assets/imgProduct").concat(File.separator).concat(fileName);
+        fileItem.write(new File(filePath));
+        return fileName;
+    }
+}
---
+++ techStore/src/main/webapp/cadProduto/altProduto.jsp
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <%@ page contentType="text/html; charset=UTF-8" %>
+    <%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>CADASTRO</title>
+    <link rel="stylesheet" href="/cadProduto/cadProduto.css">
+    <link rel="stylesheet" href="/style.css">
+</head>
+
+<body>
+    <div class="header_Border">
+        <div class="header">
+            <h1>Cadastrar produto</h1>
+            <div class="Logo">
+                <a href="/find-all-produtos"><img src="/assets/logo-techstore/logo.png" alt="logo"></a>
+            </div>
+        </div>
+    </div>
+
+    <span>${requestScope.message}</span>
+
+    <form class="form" action="/create-produto" method="post" enctype="multipart/form-data">
+        <div class="loginBox">
+            <input type="hidden" id="id" name="id" value="${param.id}">
+            <input type="text" class="input" name="nome" id="nome" placeholder="Nome do produto" value="${param.nome}">
+            <input type="number" class="input" name="preco" id="preco" placeholder="Preço unitário" step=".01" value="${param.preco}">
+            <input type="number" class="input" name="quantidade" id="quantidade" placeholder="Quantidade" value="${param.quantidade}">
+            <input type="text" class="input" name="marca" id="marca" placeholder="Marca" value="${param.marca}">
+            <div class="bloco" value="${param.categoria}">
+                <select class="Categoria" id="categoria" name="categoria">
+                    <option value="Não selecionado">Não definido</option>
+                    <option value="Placa Mãe">Placa mãe</option>
+                    <option value="Processador">Processador</option>
+                    <option value="Memoria Ram">Memória RAM</option>
+                    <option value="Armazenamento">Armazenamento interno</option>
+                    <option value="Fonte">Fonte de alimentção</option>
+                    <option value="Gabinete">Gabinete</option>
+                    <option value="Roteador">roteador</option>
+                    <option value="Periférico">Periférico</option>
+                </select>
+                <div class="insert-img" value="${param.image}">
+                    <label for="file">Choose file</label>
+                    <input type="file" name="file" id="file">
+                </div>
+            </div>
+            <textarea type="text" class="textarea" name="descricao" id="descricao" placeholder="Descrição" value="${param.descricao}"></textarea>
+
+
+            <button class="buttonLogin" type="submit">Adicionar Produto</button>
+        </div>
+    </form>
+</body>
+
+</html>
